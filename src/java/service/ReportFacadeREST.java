1a397add409df7e6691d23e4a27c2cc81e40e04c
ianest <y.estraikh@gmail.com>
2019-03-26 11:01:41 +1100

    Task 5

--- src/java/service/ReportFacadeREST.java
+++ src/java/service/ReportFacadeREST.java
@@ -6,11 +6,16 @@
 package service;
 
 import entities.Report;
+import java.sql.SQLIntegrityConstraintViolationException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.json.Json;
+import javax.json.JsonArray;
+import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
@@ -144,7 +149,7 @@ public class ReportFacadeREST extends AbstractFacade<Report> {
         query.setParameter("calorieGoal", calorieGoal);
         return query.getResultList();
     }
-    
+
     @GET
     @Path("findByUserId/{userId}")
     @Produces({"application/json"})
@@ -153,5 +158,67 @@ public class ReportFacadeREST extends AbstractFacade<Report> {
         query.setParameter("userId", userId);
         return query.getResultList();
     }
-    
+
+    //--------------------------------------------------------------------------
+    // Task 5 a
+    @GET
+    @Path("getReportByUserIdAndDate/{userId}/{reportDate}")
+    @Produces({"application/json"})
+    public Object getReportByUserIdAndDate(@PathParam("userId") int userId, @PathParam("reportDate") String reportDateString) throws SQLIntegrityConstraintViolationException {
+        try {
+            Date reportDate = new SimpleDateFormat("yyyy-MM-dd").parse(reportDateString);
+            TypedQuery<Object[]> query = em.createQuery("SELECT r.caloriesConsumed, r.caloriesBurned, r.calorieGoal FROM Report AS r WHERE r.userId.userId = :userId AND r.reportDate = :reportDate", Object[].class);
+            query.setParameter("userId", userId);
+            query.setParameter("reportDate", reportDate);
+            List<Object[]> queryList = query.getResultList();
+            if (queryList.size() > 1) {
+                throw new SQLIntegrityConstraintViolationException("userId and reportDate unique constraint was violated.");
+            }
+
+            int caloriesConsumed = (int) queryList.get(0)[0];
+            int caloriesBurned = (int) queryList.get(0)[1];
+            int remainingCalories = (int) queryList.get(0)[2] - (caloriesConsumed - caloriesBurned);
+            JsonObject jsonObject = Json.createObjectBuilder()
+                    .add("caloriesConsumed", caloriesConsumed)
+                    .add("caloriesBurned", caloriesBurned)
+                    .add("remainingCalories", remainingCalories).build();
+
+            return jsonObject;
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("reportDate must have 'yyyy-MM-dd' format.");
+        }
+    }
+
+    //--------------------------------------------------------------------------
+    // Task 5 b
+    @GET
+    @Path("getReportByUserIdBetweenDates/{userId}/{startDate}/{endDate}")
+    @Produces({"application/json"})
+    public Object getReportByUserIdBetweenDates(@PathParam("userId") int userId, @PathParam("startDate") String startDateString, @PathParam("endDate") String endDateString) {
+        try {
+            Date startDate = new SimpleDateFormat("yyyy-MM-dd").parse(startDateString);
+            Date endDate = new SimpleDateFormat("yyyy-MM-dd").parse(endDateString);
+            TypedQuery<Object[]> query = em.createQuery("SELECT r.caloriesConsumed, r.caloriesBurned, r.stepsTaken FROM Report AS r WHERE r.userId.userId = :userId AND r.reportDate BETWEEN :startDate and :endDate", Object[].class);
+            query.setParameter("userId", userId);
+            query.setParameter("startDate", startDate);
+            query.setParameter("endDate", endDate);
+            List<Object[]> queryResult = query.getResultList();
+
+            int totalCaloriesConsumed = 0;
+            int totalCaloriesBurned = 0;
+            int totalStepsTaken = 0;
+            for (Object[] row : queryResult) {
+                totalCaloriesConsumed += (int) row[0];
+                totalCaloriesBurned += (int) row[1];
+                totalStepsTaken += (int) row[2];
+            }
+            JsonObject jsonObject = Json.createObjectBuilder()
+                    .add("totalCaloriesConsumed", totalCaloriesConsumed)
+                    .add("totalCaloriesBurned", totalCaloriesBurned)
+                    .add("totalStepsTaken", totalStepsTaken).build();
+            return jsonObject;
+        } catch (ParseException e) {
+            throw new IllegalArgumentException("reportDate must have 'yyyy-MM-dd' format.");
+        }
+    }
 }
